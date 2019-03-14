ffdbc7c0cad0be0658a4f9b96be2b99ef87ea273
Tyler.Filkins <tyler.filkins@kiewit.com>
2019-03-14 14:09:51 -0500

    password crack scripts to do username/pwd correlation and reporting

---
+++ crackerjacks_correlation.ps1
@@ -0,0 +1,83 @@
+﻿<#
+ # Created By:   Tyler.Filkins
+ # Created Date: 2016-09-11
+ #
+ # Description: This script correlates usernames to passwords by cycling through two 
+ # separate lists and searching for matching hash values. Matching hash values indicate
+ # we have correctly associated a user and their password. After all matches have been
+ # found we output the new list to a text file to be used in generating reports.
+ #>
+
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+# instantiate date and other variables
+$date = (Get-Date).GetDatetimeFormats()[5]
+$crackedlist = @()
+$count = 0
+
+# import usernames and hashes
+$users_hashes = Import-Csv "C:\Temp\$date.csv"
+
+# read in list of cracked hashes
+$cracks = Get-Content "C:\Temp\cracks.txt"
+
+# for each user-hash pair, cycle through all hash-password pairs and try to find a matching hash value
+foreach($user_hash in $users_hashes)
+{
+    $found = $false
+
+    foreach($crack in $cracks)
+    {
+        # if not found
+        if(!$found)
+        {
+            # split off the hash and the password
+            $ntlm_hash = $crack.Split(":")[0]
+            $passwd = $crack.Split(":")[1]
+
+            # this checks for bible (verse:chapter) passwords and smiley faces
+            if($crack.Split(":")[2] -ne $null)
+            {
+                $passwd += $crack.Split(":")[2]
+            }
+
+            # check if the hashes match between the current user-hash pair and hash-password pair
+            if($user_hash.hash -eq $ntlm_hash)
+            {
+                # if we find a match set found to true
+                $found = $true
+
+                # save off username to use in progress bar - not necessary but whatever
+                $username = $user_hash.user
+
+                # inform the humans
+                write-host "hash match found for $username" -ForegroundColor Yellow
+
+                # build custom object and add to cracked list
+                $crackedlist += [pscustomobject] @{
+                    username = $username
+                    password = $passwd
+                }
+            }
+        }
+    }
+
+    # keep the humans informed
+    [int]$percentComplete = ($count++ / $users_hashes.count) * 100
+    Write-Progress -Activity "Enumerating crackedlist: $percentComplete%" -Status "found hash match on: $username" -PercentComplete $percentComplete
+}
+
+# once the cracked list is built, cycle through it and output each item to a text file
+$count = 0
+foreach($crack in $crackedlist)
+{
+    $crack.username + ":" + $crack.password >> "C:\Temp\$date-cracks.txt"
+    
+    [int]$percentComplete = ($count++ / $crackedlist.count) * 100
+    Write-Progress -Activity "Logging crackedlist: $percentComplete%" -Status "last user logged: $($crack.username)" -PercentComplete $percentComplete
+}
+
+$stopwatch.Stop()
+$total_mins = $stopwatch.Elapsed.TotalMinutes
+Write-Host "[*] hash correlation complete!" -ForegroundColor Green
+Write-Host "[*] total elapsed time (in minutes): $total_mins" -ForegroundColor Cyan
---
+++ crackerjacks_genreport.ps1
@@ -0,0 +1,76 @@
+﻿<#
+ # Created By:   Tyler.Filkins
+ # Created Date: 2016-09-11
+ #
+ # Description: Calculates basic metrics and generates reports on cracked passwords.
+ #>
+
+$stopwatch = [System.Diagnostics.Stopwatch]::startNew()
+$test = @()
+$cracked_allusers = @()
+$weakpwd_users = @()
+$date = (Get-Date).GetDateTimeFormats()[5]
+
+# Grab all users from the Kiewit OU and import cracked users
+$users = Get-ADUser -Filter * -Properties company,department,office,manager,whenCreated,lastLogon,lastLogonTimestamp,mail
+$cracked = Get-Content "C:\Temp\$date-cracks.txt"
+
+
+<#
+ Cycle through imported cracked users, find the corresponding AD user account, check for a null user object
+ and do some information checking/formatting, then build the cracked_allusers and weakpwd_users lists.
+#>
+foreach ($c in $cracked)
+{
+    $username = $c.Split(':')[0]
+    $password = $c.Split(':')[1]
+
+    foreach($u in $users)
+    {
+        $district = "-"
+
+        if($u.name -eq $username)
+        {
+            $lastLogon = ($u | select @{n='lastLogon';e={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}).lastLogon
+
+            $cracked_allusers += [pscustomobject] @{
+                Username=$username
+                Password=$password
+                PasswordLength=$password.Length
+                Manager=$u.Manager
+                WhenCreated=$u.whenCreated.GetDateTimeFormats()[5]
+                LastLogon=$lastLogon
+                Email=$u.mail
+            }
+
+            # check for generally weak passwords
+            elseif ($password -like "*password*" -OR
+                    $password -like "*welcome*" -OR
+                    $password -like "*summer*" -OR
+                    $password -like "*winter*" -OR
+                    $password -like "*fall*" -OR
+                    $password -like "*spring*" -OR
+                    $password -like "*$username*" -OR
+                    $password.Length -le 7)
+            {
+                $weakpwd_users += [pscustomobject] @{
+                    Username=$username
+                    Password=$password
+                }
+
+                write-host "found weak password for $username" -ForegroundColor Yellow
+            }
+        }
+    }
+
+    [int]$percentComplete = ($cracked_allusers.count / $cracked.count) * 100
+    Write-Progress -Activity "Enumerating cracked users list: $percentComplete%" -Status "on user: $username" -PercentComplete $percentComplete
+}
+
+$cracked_allusers | Export-Csv "C:\temp\$date-full-list.csv" -NoTypeInformation
+$weakpwd_users | Export-Csv "C:\Temp\wp_$date.csv" -NoTypeInformation
+
+$stopwatch.Stop()
+$total_mins = $stopwatch.Elapsed.TotalMinutes
+Write-Host "[*] finished generating reports!" -ForegroundColor Green
+Write-Host "[*] total elapsed time (in minutes): $total_mins" -ForegroundColor Cyan
